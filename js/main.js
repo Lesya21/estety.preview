9802d7204b7313560ca8cc098316bc19f350f95f
Olesya <lisenok2340@rambler.ru>
2023-05-03 20:23:25 +0300

    банер со скидкой

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
+const DISCOUNT_POPUP_SHOW_DELAY = 2000;
+const DISCOUNT_POPUP_COOKIE_NAME = 'isDiscontPopupShow';
+const DISCOUNT_POPUP_COOKIE_EXPIRE = 1000;
+
+const setCookie = (name, value, expire) => {
+   console.log(`${name}=${value};max-age=${expire}`)
+   document.cookie = `${name}=${value}; max-age=${expire}`
+}
+
+const getCookie = (name) => {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+const showDiscountPopup = () => {
+   const isPopupShown = getCookie(DISCOUNT_POPUP_COOKIE_NAME)
+   if (!isPopupShown) {
+      setTimeout(() => {
+         document.querySelector('.popup-back--discount').classList.add('open');
+         document.querySelector('.popup-content--discount').classList.add('open');
+         setCookie(DISCOUNT_POPUP_COOKIE_NAME, true, DISCOUNT_POPUP_COOKIE_EXPIRE)
+      }, DISCOUNT_POPUP_SHOW_DELAY)
+   }
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
   $('.price-slider').slick({
    		arrows: true
    });
 
+  showDiscountPopup()
+
    document.querySelector('.banner-block--sugar').addEventListener('click', () => {
    		document.querySelector('.popup-back--sugar').classList.add('open');
    		document.querySelector('.popup-content--sugar').classList.add('open');
-
-   		document.querySelector('.popup-back.open').addEventListener('click', () => {
-			document.querySelector('.popup-back--sugar').classList.remove('open');
-			document.querySelector('.popup-content--sugar').classList.remove('open');
-	 	});
    });
 
   document.querySelector('.banner-block--laser').addEventListener('click', () => {
    		document.querySelector('.popup-back--laser').classList.add('open');
    		document.querySelector('.popup-content--laser').classList.add('open');
 
-   		document.querySelector('.popup-back.open').addEventListener('click', () => {
-			document.querySelector('.popup-back--laser').classList.remove('open');
-			document.querySelector('.popup-content--laser').classList.remove('open');
-	 	});
    });
 
    document.querySelectorAll('.about-more__item-title').forEach(item => {
@@ -47,5 +67,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
    		document.querySelector('.header-menu').classList.remove('open');
    		document.querySelector('.header-burger-close').classList.remove('visible');
    });
+
+
+   document.querySelectorAll('.popup-back').forEach(item => {
+      item.addEventListener('click', ({target}) => {
+         const popup = target.closest('.popup-back')
+         if (popup && popup.classList.contains('open')) {
+            popup.classList.remove('open');
+            popup.querySelector('.popup-content').classList.remove('open')
+         }
+      })
+   })
  
 });
